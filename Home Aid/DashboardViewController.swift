55bbca7d1818bbd5a44e766aa4f97744a5f68bc5
Albrecht Oster <a.oster@online.de>
2018-02-23 17:34:47 +0100

    initial commit

---
+++ Home Aid/DashboardViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  Home Aid
+//
+//  Created by Albrecht Oster on 23.02.18.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
